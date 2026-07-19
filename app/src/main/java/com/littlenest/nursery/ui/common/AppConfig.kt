e0898f80ec5cc373d427fefe26a3d9e8b1d693b0
user <user@users-macbook-air.local>
2026-07-20 01:16:39 +0800

    Update API endpoint to GCP server

--- app/src/main/java/com/littlenest/nursery/ui/common/AppConfig.kt
+++ app/src/main/java/com/littlenest/nursery/ui/common/AppConfig.kt
@@ -1,14 +1,25 @@
 package com.littlenest.nursery.ui.common
 
+
 object AppConfig {
-    const val BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com"
+    const val BASE_URL = "http://35.223.236.169:3000"
 
     // API base (Retrofit)
-    const val API_BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com/"
+    const val API_BASE_URL = "http://35.223.236.169:3000/"
 
     // Uploads (images, files)
-    const val UPLOADS_BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com/uploads/"
+    const val UPLOADS_BASE_URL = "http://35.223.236.169:3000/uploads/"
 }
+
+//object AppConfig {
+//    const val BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com"
+//
+//    // API base (Retrofit)
+//    const val API_BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com/"
+//
+//    // Uploads (images, files)
+//    const val UPLOADS_BASE_URL = "http://ec2-13-239-60-230.ap-southeast-2.compute.amazonaws.com/uploads/"
+//}
 
 
 
